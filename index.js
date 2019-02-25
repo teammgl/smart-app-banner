bb816b1e54c6b07ccfcf3ec1415b2aa42a83be50
Gary Fullerton <fullertongr@upmc.edu>
2019-02-25 14:22:32 -0500

    More smart banner tweaks - Remove more unnecessary node modules - Add new SVG for the "close" button - Update styles to match design

--- index.js
+++ index.js
@@ -1,66 +1,19 @@
 'use strict';
 
-var extend = require('object-assign');
-var q = require('component-query');
-var doc = require('get-doc');
 var cookie = require('cookie-cutter');
 
 // IE < 11 doesn't support navigator language property.
 /* global navigator */
 var userLangAttribute = navigator.language || navigator.userLanguage || navigator.browserLanguage;
 var userLang = userLangAttribute.slice(-2) || 'us';
-var root = doc && doc.documentElement;
+var root = document.documentElement;
 
-// platform dependent functionality
-var mixins = {
-	ios: {
-		iconRels: ['apple-touch-icon-precomposed', 'apple-touch-icon'],
-		getStoreLink: function () {
-			return 'https://itunes.apple.com/' + this.options.appStoreLanguage + '/app/id' + this.appId + "?mt=8";
-		}
-	},
-	android: {
-		iconRels: ['android-touch-icon', 'apple-touch-icon-precomposed', 'apple-touch-icon'],
-		getStoreLink: function () {
-			return 'http://play.google.com/store/apps/details?id=' + this.appId;
-		}
-	},
-	windows: {
-		iconRels: ['windows-touch-icon', 'apple-touch-icon-precomposed', 'apple-touch-icon'],
-		getStoreLink: function () {
-			return 'http://www.windowsphone.com/s?appid=' + this.appId;
-		}
-	}
-};
-
+// options = {daysHidden, daysReminder, title, author, button, store, appIds, icon, [close], [show]}
 var SmartBanner = function (options) {
-	this.options = extend({}, {
-		daysHidden: 15,
-		daysReminder: 90,
-		appStoreLanguage: userLang, // Language code for App Store
-		button: 'OPEN', // Text for the install button
-		store: {
-			ios: 'On the App Store',
-			android: 'In Google Play',
-			windows: 'In the Windows Store'
-		},
-		appIds: {},
-		price: {
-			ios: 'FREE',
-			android: 'FREE',
-			windows: 'FREE'
-		},
-		theme: '', // put platform type ('ios', 'android', etc.) here to force single theme on all device
-		icon: '', // full path to icon image if not using website icon image
-		force: '', // put platform type ('ios', 'android', etc.) here for emulation
+	this.options = options;
+	this.options.appStoreLanguage = userLang;
 
-	}, options || {});
-
-	if (this.options.force) {
-		this.type = this.options.force;
-	} else if (navigator.userAgent.match(/IEMobile/i)) {
-		this.type = 'windows';
-	} else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+	if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
 		this.type = 'ios';
 	} else if (navigator.userAgent.match(/Android/i)) {
 		this.type = 'android';
@@ -68,11 +21,10 @@ var SmartBanner = function (options) {
 
 	this.appId = this.type && this.options.appIds[this.type];
 	// Don't show banner on ANY of the following conditions:
-	// - device os is not supported,
-	// - user is on mobile safari for ios 6 or greater (iOS >= 6 has native support for SmartAppBanner)
+	// - appId not defined,
 	// - running on standalone mode
 	// - user dismissed banner
-	var unsupported = !this.type || !this.options.store[this.type] || !this.appId;
+	var unsupported = !this.appId;
 	var runningStandAlone = navigator.standalone;
 	var userDismissed = cookie.get(this.appId + '-smartbanner-closed');
 	var userInstalled = cookie.get(this.appId + '-smartbanner-installed');
@@ -80,8 +32,6 @@ var SmartBanner = function (options) {
 	if (unsupported || runningStandAlone || userDismissed || userInstalled) {
 		return;
 	}
-
-	extend(this, mixins[this.type]);
 
 	this.create();
 	this.show();
@@ -91,51 +41,45 @@ SmartBanner.prototype = {
 	constructor: SmartBanner,
 
 	create: function () {
-		var link = this.getStoreLink();
-		var inStore = this.options.price[this.type] + ' - ' + this.options.store[this.type];
-		var icon;
+		var sb = document.createElement('div');
 
-		if (this.options.icon) {
-			icon = this.options.icon;
-		} else {
-			for (var i = 0; i < this.iconRels.length; i++) {
-				var rel = q('link[rel="' + this.iconRels[i] + '"]');
-
-				if (rel) {
-					icon = rel.getAttribute('href');
-					break;
-				}
-			}
-		}
-
-		var sb = doc.createElement('div');
-		var theme = this.options.theme || this.type;
-
-		sb.className = 'smartbanner smartbanner-' + theme;
+		sb.className = 'smartbanner smartbanner-' + this.type;
 		sb.innerHTML = '<div class="smartbanner-container">' +
-							'<a href="javascript:void(0);" class="smartbanner-close">&times;</a>' +
-							'<span class="smartbanner-icon" style="background-image: url(' + icon + ')"></span>' +
+							'<a href="javascript:void(0);" class="smartbanner-close">' +
+								'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">' +
+									'<path d="M8 8l8 8m0-8l-8 8" fill="none" stroke="#222521" stroke-width="1.231" stroke-linecap="round"/>' +
+								'</svg>' +
+							'</a>' +
+							'<span class="smartbanner-icon" style="background-image: url(' + this.options.icon + ')"></span>' +
 							'<div class="smartbanner-info">' +
 								'<div class="smartbanner-title">' + this.options.title + '</div>' +
 								'<div>' + this.options.author + '</div>' +
-								'<span>' + inStore + '</span>' +
+								'<span>' + this.options.store[this.type] + '</span>' +
 							'</div>' +
-							'<a href="' + link + '" class="smartbanner-button">' +
+							'<a href="' + this.link() + '" class="smartbanner-button">' +
 								'<span class="smartbanner-button-text">' + this.options.button + '</span>' +
 							'</a>' +
 						'</div>';
 
 		// there isn’t neccessary a body
-		if (doc.body) {
-			doc.body.appendChild(sb);
-		}		else if (doc) {
-			doc.addEventListener('DOMContentLoaded', function () {
-				doc.body.appendChild(sb);
+		if (document.body) {
+			document.body.appendChild(sb);
+		}		else if (document) {
+			document.addEventListener('DOMContentLoaded', function () {
+				document.body.appendChild(sb);
 			});
 		}
 
-		q('.smartbanner-button', sb).addEventListener('click', this.install.bind(this), false);
-		q('.smartbanner-close', sb).addEventListener('click', this.close.bind(this), false);
+		document.querySelector('.smartbanner-button').addEventListener('click', this.install.bind(this), false);
+		document.querySelector('.smartbanner-close').addEventListener('click', this.close.bind(this), false);
+	},
+	link: function () {
+		switch(this.type) {
+			case 'ios':
+				return 'https://itunes.apple.com/' + this.options.appStoreLanguage + '/app/id' + this.appId + "?mt=8";
+			case 'android':
+				return 'http://play.google.com/store/apps/details?id=' + this.appId;
+		}
 	},
 	hide: function () {
 		root.classList.remove('smartbanner-show');
@@ -151,24 +95,18 @@ SmartBanner.prototype = {
 		}
 	},
 	close: function () {
+		this.setCookie('closed', this.options.daysHidden);
 		this.hide();
-		cookie.set(this.appId + '-smartbanner-closed', 'true', {
-			path: '/',
-			expires: new Date(Number(new Date()) + (this.options.daysHidden * 1000 * 60 * 60 * 24))
-		});
-		if (typeof this.options.close === 'function') {
-			return this.options.close();
-		}
 	},
 	install: function () {
+		this.setCookie('installed', this.options.daysReminder);
 		this.hide();
-		cookie.set(this.appId + '-smartbanner-installed', 'true', {
+	},
+	setCookie: function (action, daysToExpire) {
+		cookie.set(this.appId + '-smartbanner-' + action, 'true', {
 			path: '/',
-			expires: new Date(Number(new Date()) + (this.options.daysReminder * 1000 * 60 * 60 * 24))
+			expires: new Date(Number(new Date()) + (daysToExpire * 1000 * 60 * 60 * 24))
 		});
-		if (typeof this.options.close === 'function') {
-			return this.options.close();
-		}
 	}
 };
 
